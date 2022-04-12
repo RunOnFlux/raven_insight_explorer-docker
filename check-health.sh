915eafff7c8ac31184eb5d244a782f80932e746a
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-04-12 18:17:34 +0200

    Create check-health.sh

---
+++ check-health.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+CURRENT_NODE_HEIGHT=$(raven-cli -datadir=/root/.ravencore/data getblockchaininfo | jq -r .blocks)
+if ! egrep -o "^[0-9]+$" <<< "$CURRENT_NODE_HEIGHT" &>/dev/null; then
+  echo "Daemon not working correct..."
+  exit 1
+else
+  echo "Daemon working correct..."
+  exit
+fi
